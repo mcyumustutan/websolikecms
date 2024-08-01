f90dc6c2ed5e016fb3a15d36180178efe2a6a8b1
Mcan <mehmetcy01@gmail.com>
2024-08-01 11:46:59 +0300

    başlıklar düzenlendi

--- resources/views/components/newslists.blade.php
+++ resources/views/components/newslists.blade.php
@@ -1,4 +1,4 @@
-<section class="bottom-padding section-bg-before-two mt-40">
+<section class="bottom-padding section-bg-before-two mt-40 about-area bottom-padding1 position-relative">
     <div class="container">
 
         <div class="position-relative">
@@ -195,4 +195,7 @@
             </div>
         </div>
     </div>
-</section> 
+    <div class="shape-bg-about">
+        <img src="{{asset('images/icon/bg-shape-2.png')}}" alt="{{ $settings['site-basligi'] }}">
+    </div>
+</section>
--- resources/views/components/stories.blade.php
+++ resources/views/components/stories.blade.php
@@ -1,4 +1,4 @@
-<section class="special-area position-relative pt-4">
+<section class="special-area position-relative pt-4 mt-4">
     <div class="container">
         <div class="row">
             <div id="stories" class="storiesWrapper d-flex justify-content-center"></div>
