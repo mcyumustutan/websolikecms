af600b8349c2abceae090ef3f042eba21f59e0c7
Mcan <mehmetcy01@gmail.com>
2024-08-18 09:27:07 +0300

    yaklaşan etkinlşk sıralaması

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -150,7 +150,7 @@ class PageController extends Controller
                 TemplateType::Event->value
             ])
                 // ->where('display_date', '>=', Carbon::today())
-                ->orderBy('display_date', 'asc')
+                ->orderBy('display_date', 'desc')
                 ->take(6)->get(),
 
             'stories' => Page::where('is_publish', true)
