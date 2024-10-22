be8770c51dcd8bb580ba7f7021f29248bafe7892
Ekonomikmama <bilgiislem@ekonomikmama.com>
2024-10-22 22:26:49 +0300

    Sayfalar için yayın tarihi kutulardan kaldırıldı

--- resources/views/page/page.blade.php
+++ resources/views/page/page.blade.php
@@ -70,8 +70,71 @@
     </div>
 </section>
 
-@if($page->has_subpages)
-@include('components/pagination')
+@if($page->has_subpages && count($subPages['data'])>0)
+<section class="news-area ">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-12 col-lg-12">
+                <div class="position-relative mb-60 mt-60">
+                    <h4 class="title">
+                        {{ __('websolike.Related Pages')}}
+                    </h4>
+                </div>
+            </div>
+        </div>
+        <div class="row g-4">
+
+            @foreach ($subPages['data'] as $subPage)
+
+
+            <div class="col-xl-3 col-lg-3 col-sm-6 ">
+                <article class="news-card-two">
+                    <figure class="news-banner-two imgEffect   justify-content-center align-items-center" style="min-height: 260px;">
+                        <a href="{{$subPage['url']}}">
+                            <img src="{{ $subPage['cover'] }}" alt="{{ $subPage['title'] }}">
+                        </a>
+                    </figure>
+
+                    <div class="news-content">
+
+                        <h3 class="title">
+                            <a href="{{$subPage['url']}}">
+                                {{$subPage['title']}}
+                            </a>
+
+                            @if($subPage['highlited_value_1'])
+                            <div class="count">
+                                {{$subPage['highlited_value_1']}}
+                            </div>
+                            @endif
+                        </h3>
+
+                    </div>
+                </article>
+            </div>
+
+            @endforeach
+        </div>
+
+
+        <div class="col-12 text-center">
+
+            <div class="d-flex justify-content-center mt-4">
+                <nav class="pagination-container">
+                    <div class="pagination">
+                        <span class="pagination-inner">
+                            @foreach ($subPages['links'] as $link)
+                            <a class="{{ $link['active'] ? 'pagination-active' : '' }}" href="{{$link['url']}}">
+                                {{__($link['label']) }}
+                            </a>
+                            @endforeach
+                        </span>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+</section>
 @endif
 
 @endsection
